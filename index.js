4a1c04fe2951490cc9105402b5446d392846af04
blaisepalombo <156847922+blaisepalombo@users.noreply.github.com>
2026-07-09 16:07:09 -0600

    Show onboarding after login

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-import { registerRootComponent } from 'expo';
+import { StyleSheet, View } from "react-native";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+import OnboardingOverlay from "./OnboardingOverlay";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+function Root() {
+  return (
+    <View style={styles.root}>
+      <App />
+      <OnboardingOverlay />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => Root);
 // It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+// the environment is set up appropriately.
+registerRootComponent(Root);
